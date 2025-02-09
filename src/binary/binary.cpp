8a41bccb27116a64c4f745fb47bd9ec2e5fcc08a
Nestyles <tom.sancho@epitech.eu>
2025-02-10 02:32:42 +0800

    [FIX] Crash when the binary was not an executable format

--- src/binary/binary.cpp
+++ src/binary/binary.cpp
@@ -5,6 +5,10 @@
 
 Binary::Binary(const std::string path) {
     _lief_binary = LIEF::Parser::parse(path);
+    if (_lief_binary == nullptr) {
+        std::cerr << "Failed to parse the binary" << std::endl;
+        exit(1);
+    }
     _metadata = std::make_unique<BinaryMetadata>(_lief_binary, path);
 }
 
