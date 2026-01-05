c6bdf4a41db43794bed3cce1e31a3669b004dea7
Lucas TAILLAN <lucas.taillan@epitech.eu>
2026-01-05 16:24:34 +0100

    [ADD] current list for commits

--- include/version/VersionControl.hpp
+++ include/version/VersionControl.hpp
@@ -3,7 +3,9 @@
 #include <string>
 #include <set>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
+#include "binary/binary.hpp"
 
 class VCS {
    public:
@@ -17,8 +19,8 @@ class VCS {
         std::string get_commit_msg() const { return _commit_msg; };
         std::string get_commit_time() const { return _commit_time; };
 
-        void serialize_commit(const std::string commit_path, const std::string commit_msg);
-        void deserialize_commit(const std::string commit_path);
+        void serialize_commit(const std::string commit_path, const std::string commit_msg, std::ostream &o);
+        void deserialize_commit(const std::string commit_path, std::istream &i);
        private:
         std::string _commit_path;
         std::string _commit_msg;
@@ -26,39 +28,106 @@ class VCS {
         int _commit_number;
     };
 
+    struct renamedFn {
+        size_t function_id;
+        std::string new_name;
+    };
+
+    class Commit {
+       public:
+        Commit(std::filesystem::path path): _info(path.string()) {
+            _path = path;
+        };
+        Commit(const std::string staging_path): _info(staging_path){
+            _path = staging_path;
+
+        }
+        ~Commit() {};
+        std::filesystem::path _path;
+        CommitInfo _info;
+
+        std::vector<renamedFn> _fn;
+
+        enum Status {
+            STAGING = 0,
+            COMMITTED = 1
+        };
+
+        Status status = STAGING;
+
+        void set_fn_list(std::vector<Binary::Function> renamed_list) {
+            for (auto &func: renamed_list) {
+                renamedFn fn;
+            }
+
+        }
+        void serialize_commit(const std::string commit_path, const std::string commit_msg);
+        void deserialize_commit(const std::string commit_path);
+    };
+
     struct CommitComparator {
-        bool operator()(const std::pair<std::filesystem::path, CommitInfo>& lhs, const  std::pair<std::filesystem::path, CommitInfo>& rhs) const {
-            return lhs.first.string() < rhs.first.string();
+        bool operator()(const VCS::Commit & lhs, const VCS::Commit & rhs) const {
+            return lhs._path.string() < rhs._path.string();
         }
     };
 
-    VCS(const std::string project_path)
+    VCS(const std::string project_path, std::vector<Binary::Function> list)
         : _version_path(std::filesystem::path(project_path) / ".daic"),
           _staging_path(_version_path / "staging"),
-          _commit_path(_version_path / "commits")
+          _commit_path(_version_path / "commits"),
+          _current_path(_version_path / "current")
     {
-        if (isFileTracked()) {
-            std::cout << "Version control system found";
-        } else {
-            init();
-        }
         _db_path = std::filesystem::path(project_path) / "1.db";
+        if (isFileTracked()) {
+            std::cout << "Version control system found\n";
+        } else {
+            init(list);
+        }
         _commits = sort_commit_folders();
     };
     ~VCS() {};
 
-    void add();
+    void add(std::vector<Binary::Function>);
     void commit(const std::string);
-    std::set<std::pair<std::filesystem::path, CommitInfo>, CommitComparator> _commits;
+    std::set<VCS::Commit*> _commits;
 
+    std::filesystem::path getCurrentPath() { return _current_path;};
+    void serializeCurrentList(std::vector<Binary::Function> list) {
+        std::ofstream out(std::format("{}/functions.db", _current_path.string(), std::ios::binary));
+        if (!out)
+            throw std::runtime_error("Failed to open file for writing");
+        size_t func_size = list.size();
+        out.write(reinterpret_cast<const char*>(&func_size), sizeof(func_size));
+        for (auto &func: list) {
+            func.serialize(out);
+        }
+    }
+    std::vector<Binary::Function> deserializeCurrentList() {
+        std::ifstream in(std::format("{}/functions.db", _current_path.string()), std::ios::binary);
+        std::vector<Binary::Function> list;
+        if (!in) {
+            return list;
+        }
+        size_t func_size;
+        in.read(reinterpret_cast<char*>(&func_size), sizeof(func_size));
+        for (size_t i = 0; i != func_size; i++) {
+            Binary::Function func;
+            func.deserialize(in);
+            list.push_back(func);
+        }
+        return list;
+    }
 
    private:
     std::filesystem::path _version_path;;
     std::filesystem::path _staging_path;
     std::filesystem::path _commit_path;
+    std::filesystem::path _current_path;
     std::filesystem::path _db_path;
 
-    void init();
+    void init(std::vector<Binary::Function>);
     bool isFileTracked();
-    std::set<std::pair<std::filesystem::path, CommitInfo>, CommitComparator> sort_commit_folders();
+    void copy_current_db();
+    void copy_last_commit_db();
+    std::set<VCS::Commit*> sort_commit_folders();
 };
--- src/version/VersionControl.cpp
+++ src/version/VersionControl.cpp
@@ -3,73 +3,96 @@
 #include <fstream>
 #include "version/VersionControl.hpp"
 
-void VCS::init()
+void VCS::init(std::vector<Binary::Function> list)
 {
-    std::cout << "no versions found, initializing version control";
+    std::cout << "no versions found, initializing version control\n";
     try {
         std::filesystem::create_directory(_version_path);
         std::filesystem::create_directory(_staging_path);
         std::filesystem::create_directory(_commit_path);
+        std::filesystem::create_directory(_current_path);
+        serializeCurrentList(list);
     } catch (const std::filesystem::filesystem_error& e) {
         std::cerr << "Error creating directories: " << e.what() << std::endl;
     }
 }
 
+void VCS::copy_current_db()
+{
+    std::filesystem::copy_file(_db_path, _current_path / "1.db",
+            std::filesystem::copy_options::overwrite_existing);
+}
+
+void VCS::copy_last_commit_db()
+{
+    if (!_commits.empty()) {
+        auto lcommit = _commits.begin();
+        Commit *last_commit = *lcommit;
+        std::filesystem::copy_file(last_commit->_path / "1.db", _current_path / "1.db");
+    }
+}
+
 bool VCS::isFileTracked()
 {
+    std::cout << "tracking files\n";
     return std::filesystem::exists(_version_path) &&
+           std::filesystem::exists(_current_path) &&
            std::filesystem::exists(_staging_path) &&
            std::filesystem::exists(_commit_path);;
 }
 
-std::set<std::pair<std::filesystem::path, VCS::CommitInfo>, VCS::CommitComparator> VCS::sort_commit_folders()
+std::set<VCS::Commit *> VCS::sort_commit_folders()
 {
-    std::set<std::pair<std::filesystem::path, VCS::CommitInfo>, VCS::CommitComparator> commit_folders;
+    std::set<VCS::Commit*> commit_folders;
     for (const auto& entry : std::filesystem::directory_iterator(_commit_path)) {
         if (!std::filesystem::is_directory(entry.path())) {
             continue;
         }
-        CommitInfo info(entry.path().string());
-        commit_folders.insert({entry.path(), info});
+        Commit *commit = new Commit(entry.path().string());
+        commit_folders.insert(commit);
     }
     return commit_folders;
 }
 
-void VCS::add()
+void VCS::add(std::vector<Binary::Function> renamed_func_list)
 {
-    std::cout << "Adding changes to Staging in " << _staging_path.string();
-    std::filesystem::copy_file(_db_path, _staging_path / "1.db", std::filesystem::copy_options::overwrite_existing);
-}
+    std::cout << "Adding changes to Staging in " << _staging_path.string() << std::endl;
 
-void VCS::commit(const std::string commit_msg)
-{
+    auto commit_staging_path = _staging_path / "commit_0";
+    Commit commit(commit_staging_path.string());
+    commit.serialize_commit(commit._path.string(), "");
     if (_commits.empty()) {
         std::filesystem::path new_commit_path = _commit_path / "commit_0";
-        std::filesystem::create_directory(new_commit_path);
-        std::filesystem::copy_file(_staging_path / "1.db", new_commit_path / "1.db");
-        CommitInfo info(new_commit_path.string());
-        info.serialize_commit(new_commit_path.string(), commit_msg);
-        _commits.insert({new_commit_path, info});
+        Commit *commit = new Commit(new_commit_path);
+        commit->serialize_commit(_staging_path.string(), "");
+        _commits.insert(commit);
         return;
     }
-    std::string last_commit_str= _commits.rbegin()->first.string();
-    int last_commit = std::stoi(last_commit_str.substr(last_commit_str.find_last_of("_") + 1));
-    std::filesystem::path new_commit_path = _commit_path / ("commit_" + std::to_string(last_commit + 1));
-    std::filesystem::create_directory(new_commit_path);
-    std::filesystem::copy_file(_staging_path / "1.db", new_commit_path / "1.db");
-    CommitInfo info(new_commit_path.string());
-    info.serialize_commit(new_commit_path.string(), commit_msg);
-    _commits.insert({new_commit_path, info});
+    auto rit = _commits.rbegin();
+    Commit *last_commit = *rit;
+    if (last_commit->status == Commit::COMMITTED) {
+        std::string last_commit_str= last_commit->_path.string();
+        int last_commit_i = std::stoi(last_commit_str.substr(last_commit_str.find_last_of("_") + 1));
+        std::filesystem::path new_commit_path = _commit_path / ("commit_" + std::to_string(last_commit_i + 1));
+        std::filesystem::create_directory(new_commit_path);
+        Commit *commit = new Commit(new_commit_path);
+        commit->serialize_commit(commit->_path.string(), commit_msg);
+        _commits.insert(commit);
+    } else {
+        last_commit->serialize_commit(commit->)
+    }
 }
 
-void VCS::CommitInfo::serialize_commit(const std::string commit_path, const std::string commit_msg)
+void VCS::commit(const std::string commit_msg, Commit *commit)
+{
+
+}
+
+void VCS::CommitInfo::serialize_commit(const std::string commit_path, const std::string commit_msg, std::ostream &out)
 {
     _commit_msg = commit_msg;
     _commit_time = std::to_string(std::time(nullptr));
     _commit_number = std::stoi(commit_path.substr(commit_path.find_last_of("_") + 1));
-
-    std::filesystem::path commit_file = std::filesystem::path(commit_path) / "commit_info.info";
-    std::ofstream out(commit_file.string(), std::ios::binary);
 
     out.write(reinterpret_cast<const char*>(&_commit_number), sizeof(_commit_number));
 
@@ -80,14 +103,10 @@ void VCS::CommitInfo::serialize_commit(const std::string commit_path, const std:
     uint32_t timeSize = _commit_time.size();
     out.write(reinterpret_cast<const char*>(&timeSize), sizeof(timeSize));
     out.write(_commit_time.data(), timeSize);
-    out.close();
 }
 
-void VCS::CommitInfo::deserialize_commit(const std::string commit_path)
+void VCS::CommitInfo::deserialize_commit(const std::string commit_path, std::istream &in)
 {
-    std::filesystem::path commit_file = std::filesystem::path(commit_path) / "commit_info.info";
-    std::ifstream in(commit_file.string(), std::ios::binary);
-
     in.read(reinterpret_cast<char*>(&_commit_number), sizeof(_commit_number));
 
     uint32_t msgSize;
@@ -99,5 +118,45 @@ void VCS::CommitInfo::deserialize_commit(const std::string commit_path)
     in.read(reinterpret_cast<char*>(&timeSize), sizeof(timeSize));
     _commit_time.resize(timeSize);
     in.read(_commit_time.data(), timeSize);
+}
+
+void VCS::Commit::serialize_commit(const std::string commit_path, const std::string commit_msg)
+{
+    std::filesystem::path commit_file = std::filesystem::path(commit_path) / "commit_info.info";
+    std::ofstream out(commit_file.string(), std::ios::binary);
+    this->_info.serialize_commit(commit_path, commit_msg, out);
+
+    uint32_t fnSize = _fn.size();
+    out.write(reinterpret_cast<const char*>(&fnSize), sizeof(fnSize));
+    for (auto &fn: this->_fn) {
+        out.write(reinterpret_cast<const char*>(&fn.function_id), sizeof(fn.function_id));
+
+        uint32_t fnNameSize = fn.new_name.size();
+        out.write(reinterpret_cast<const char*>(&fnNameSize), sizeof(fnNameSize));
+        out.write(fn.new_name.data(), fnNameSize);
+    }
+    out.close();
+}
+
+void VCS::Commit::deserialize_commit(const std::string commit_path)
+{
+    std::filesystem::path commit_file = std::filesystem::path(commit_path) / "commit_info.info";
+    std::ifstream in(commit_file.string(), std::ios::binary);
+    this->_info.deserialize_commit(commit_path, in);
+    uint32_t fnSize;
+    in.read(reinterpret_cast<char*>(&fnSize), sizeof(fnSize));
+
+    for (uint32_t i = 0; i < fnSize; i++) {
+        renamedFn fn;
+        in.read(reinterpret_cast<char*>(&fn.function_id), sizeof(fn.function_id));
+
+        uint32_t fnNameSize;
+        in.read(reinterpret_cast<char*>(&fnNameSize), sizeof(fnNameSize));
+
+        fn.new_name.resize(fnNameSize);
+        in.read(fn.new_name.data(), fnNameSize);
+        _fn.push_back(fn);
+    }
+
     in.close();
 }
