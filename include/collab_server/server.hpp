05b5694881bfe8275058ac063698d3d61c6d98dc
Nestyles <tom.sancho@epitech.eu>
2026-01-31 16:37:30 +0100

    [REWORK] All collab server replaced from apca to Qt Sockets

--- include/collab_server/server.hpp
+++ include/collab_server/server.hpp
@@ -3,34 +3,44 @@
 #include <string>
 #include <vector>
 #include <mutex>
+#include <memory>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QObject>
 
-class CollabServer
+class CollabServer : public QObject
 {
+    Q_OBJECT
+
     public:
         struct statusRes {
             bool code;
             std::string message;
         };
 
-        CollabServer() {}
+        CollabServer(QObject* parent = nullptr);
         ~CollabServer();
         statusRes startServerThread(int port);
         statusRes connect_to_peer(const std::string& ip, int port);
         void send_file_to_peers(std::string file_path, std::string project_name);
-        bool get_is_online() {
+        bool get_is_online() const {
             return _is_online;
         }
-        int get_server_fd() {
-            return _server_fd;
-        }
+
+    private slots:
+        void on_new_connection();
+        void on_peer_connected();
+        void on_peer_disconnected();
+        void on_peer_ready_read();
+        void on_peer_error();
 
     private:
-        void send_message_to_peers(const std::string& message, int sender_fd);
-        void handle_peer(int peer_fd);
-        void check_peers_connection();
+        void send_message_to_peers(const std::string& message, QTcpSocket* sender_socket);
+        void handle_peer_data(QTcpSocket* peer_socket, const std::string& message);
         statusRes start_server(int port);
-        std::vector<int> _peers;
+
+        std::unique_ptr<QTcpServer> _server;
+        std::vector<std::unique_ptr<QTcpSocket>> _peers;
         std::mutex _peers_mutex;
-        int _server_fd;
         bool _is_online;
 };
--- src/collab_server/server.cpp
+++ src/collab_server/server.cpp
@@ -1,138 +1,169 @@
 #include "collab_server/server.hpp"
 
 #include <iostream>
-#include <thread>
 #include <fstream>
 #include <sstream>
-//#include <arpa/inet.h>
 #include <filesystem>
+#include <QHostAddress>
+#include <QByteArray>
 
-// todo: remove logs
-/*
+CollabServer::CollabServer(QObject* parent)
+    : QObject(parent), _server(nullptr), _is_online(false)
+{
+}
+
 CollabServer::~CollabServer() {
-    //close(_server_fd);
+    std::lock_guard<std::mutex> lock(_peers_mutex);
+    for (auto& peer : _peers) {
+        if (peer && peer->state() == QTcpSocket::ConnectedState) {
+            peer->disconnectFromHost();
+        }
+    }
+    if (_server) {
+        _server->close();
+    }
 }
 
 CollabServer::statusRes CollabServer::connect_to_peer(const std::string& ip, int port) {
-    //int peer_fd = socket(AF_INET, SOCK_STREAM, 0);
+    auto peer_socket = std::make_unique<QTcpSocket>();
 
-    //if (peer_fd < 0)
-    //    return { false, "Error: Failed to create client socket" };
+    connect(peer_socket.get(), &QTcpSocket::connected, this, &CollabServer::on_peer_connected);
+    connect(peer_socket.get(), &QTcpSocket::disconnected, this, &CollabServer::on_peer_disconnected);
+    connect(peer_socket.get(), &QTcpSocket::readyRead, this, &CollabServer::on_peer_ready_read);
+    connect(peer_socket.get(), &QTcpSocket::errorOccurred, this, [this]() { on_peer_error(); });
 
-    //sockaddr_in peer_addr;
-    //peer_addr.sin_family = AF_INET;
-    //peer_addr.sin_port = htons(port);
-    //inet_pton(AF_INET, ip.c_str(), &peer_addr.sin_addr);
+    peer_socket->connectToHost(QString::fromStdString(ip), port);
 
-    //if (connect(peer_fd, (struct sockaddr *) &peer_addr, sizeof(peer_addr)) < 0)
-    //    return { false, "Error: Failed to connect to peer" };
+    if (!peer_socket->waitForConnected(5000)) {
+        return { false, "Error: Failed to connect to peer" };
+    }
 
-    //std::lock_guard<std::mutex> lock(_peers_mutex);
-    //_peers.push_back(peer_fd);
+    {
+        std::lock_guard<std::mutex> lock(_peers_mutex);
+        _peers.push_back(std::move(peer_socket));
+    }
 
-    //std::thread handle_peer_thread([this, peer_fd]() {
-    //    this->handle_peer(peer_fd);
-    //});
-    //handle_peer_thread.detach();
-
-    //_is_online = true;
-    return {true, "Successfully connected to" + ip + ":" };
+    _is_online = true;
+    return {true, "Successfully connected to " + ip + ":" + std::to_string(port)};
 }
 
 void CollabServer::send_file_to_peers(std::string file_path, std::string project_name) {
     std::ifstream text(file_path);
     std::stringstream buffer;
     size_t pos = file_path.find_last_of("/\\");
-    std::string new_file_path= file_path.substr(0, pos + 1) + project_name + "_" + file_path.substr(pos + 1);
+    std::string new_file_path = file_path.substr(0, pos + 1) + project_name + "_" + file_path.substr(pos + 1);
 
     buffer << "DaicFile " << new_file_path << '\n' << text.rdbuf();
     std::string str = buffer.str();
 
-    send_message_to_peers(str, _server_fd);
+    send_message_to_peers(str, nullptr);
 }
 
-void CollabServer::send_message_to_peers(const std::string &message, int sender_peer_fd) {
+void CollabServer::send_message_to_peers(const std::string &message, QTcpSocket* sender_socket) {
     std::lock_guard<std::mutex> lock(_peers_mutex);
     std::cout << "Sending to " << _peers.size() << " peers" << std::endl;
 
-    for (auto peer_fd : _peers) {
-        if (peer_fd != sender_peer_fd) {
-            std::cout << "Sending to peer " << peer_fd << std::endl;
-            //send(peer_fd, message.c_str(), message.size(), 0);
+    QByteArray data(message.c_str(), message.size());
+
+    for (auto& peer : _peers) {
+        if (peer && peer.get() != sender_socket && peer->state() == QTcpSocket::ConnectedState) {
+            std::cout << "Sending to peer" << std::endl;
+            peer->write(data);
+            peer->flush();
         }
     }
 }
 
-void CollabServer::handle_peer(int peer_fd) {
-    std::vector<char> buffer(512000); // temp
-    std::cout << "Peer connected: " << peer_fd << std::endl;
+void CollabServer::on_peer_connected() {
+    std::cout << "Peer connected" << std::endl;
+    _is_online = true;
+}
+
+void CollabServer::on_peer_disconnected() {
+    std::cout << "Peer disconnected" << std::endl;
+    std::lock_guard<std::mutex> lock(_peers_mutex);
+    _peers.erase(
+        std::remove_if(_peers.begin(), _peers.end(),
+            [](const std::unique_ptr<QTcpSocket>& peer) {
+                return !peer || peer->state() != QTcpSocket::ConnectedState;
+            }),
+        _peers.end()
+    );
+
+    if (_peers.empty()) {
+        _is_online = false;
+    }
+}
+
+void CollabServer::on_peer_ready_read() {
+    QTcpSocket* peer_socket = qobject_cast<QTcpSocket*>(sender());
+    if (!peer_socket) return;
+
+    QByteArray data = peer_socket->readAll();
+    std::string message(data.constData(), data.size());
+
+    if (message.find("DaicFile") == 0) {
+        std::string first_line = message.substr(0, message.find("\n"));
+        std::string file_name = first_line.substr(first_line.find_last_of("/\\") + 1);
+        std::string file_content = message.substr(first_line.size() + 1);
+
+        std::cout << "Received file: " << file_name << std::endl;
+
+        const std::filesystem::path tmp_dir_path = std::filesystem::temp_directory_path() / "DaiC";
+        const std::filesystem::path file_tmp_path = tmp_dir_path / file_name;
+
+        std::filesystem::create_directory(tmp_dir_path);
+        std::fstream s {file_tmp_path, std::ios::binary | std::ios::trunc | std::ios::in | std::ios::out};
+        s.write(file_content.c_str(), file_content.size());
+        s.seekp(0);
+    }
+
+    handle_peer_data(peer_socket, message);
+}
+
+void CollabServer::on_peer_error() {
+    QTcpSocket* peer_socket = qobject_cast<QTcpSocket*>(sender());
+    if (peer_socket) {
+        std::cout << "Peer error: " << peer_socket->errorString().toStdString() << std::endl;
+    }
+}
+
+void CollabServer::handle_peer_data(QTcpSocket* peer_socket, const std::string& message) {
+    // This function is a placeholder for handling peer data
+    // Implement custom logic here as needed
+}
+
+void CollabServer::on_new_connection() {
+    QTcpSocket* peer_socket = _server->nextPendingConnection();
+    if (!peer_socket) return;
+
+    auto peer = std::make_unique<QTcpSocket>();
+    peer.reset(peer_socket);
+
+    connect(peer.get(), &QTcpSocket::connected, this, &CollabServer::on_peer_connected);
+    connect(peer.get(), &QTcpSocket::disconnected, this, &CollabServer::on_peer_disconnected);
+    connect(peer.get(), &QTcpSocket::readyRead, this, &CollabServer::on_peer_ready_read);
+    connect(peer.get(), &QTcpSocket::errorOccurred, this, [this]() { on_peer_error(); });
+
+    {
+        std::lock_guard<std::mutex> lock(_peers_mutex);
+        _peers.push_back(std::move(peer));
+    }
 
     _is_online = true;
-    while (true) {
-        std::fill(buffer.begin(), buffer.end(), 0);
-        //int bytes_received = recv(peer_fd, buffer.data(), buffer.size(), 0);
-        int bytes_received = 0;
-
-        if (bytes_received < 1) {
-            std::cout << "Peer disconnected: " << peer_fd << "\n";
-            std::lock_guard<std::mutex> lock(_peers_mutex);
-            //close(peer_fd);
-            break;
-        }
-        std::string message(buffer.data(), bytes_received);
-
-        if (message.starts_with("DaicFile")) {
-            std::string first_line = message.substr(0, message.find("\n"));
-            std::string file_name = first_line.substr(first_line.find_last_of("/\\") + 1);
-            std::string file_content = message.substr(first_line.size() + 1);
-
-            std::cout << "Received file: " << file_name << std::endl;
-
-            const std::filesystem::path tmp_dir_path = std::filesystem::temp_directory_path() / "DaiC";
-            const std::filesystem::path file_tmp_path = tmp_dir_path / file_name;
-
-            std::filesystem::create_directory(tmp_dir_path);
-            std::fstream s {file_tmp_path, s.binary | s.trunc | s.in | s.out};
-            s.write(file_content.c_str(), file_content.size());
-            s.seekp(0);
-        }
-    }
-}
-
-void CollabServer::check_peers_connection() {
-    while (true) {
-        //sockaddr_in peer_addr;
-        //socklen_t peer_len = sizeof(peer_addr);
-        //int peer_fd = accept(_server_fd, (struct sockaddr *) &peer_addr, &peer_len);
-
-        //std::lock_guard<std::mutex> lock(_peers_mutex);
-        //_peers.push_back(peer_fd);
-
-        //std::thread handle_peer_thread([this, peer_fd]() {
-        //    this->handle_peer(peer_fd);
-        //});
-        //handle_peer_thread.detach();
-    }
+    std::cout << "New peer connected" << std::endl;
 }
 
 CollabServer::statusRes CollabServer::start_server(int port) {
-    //_server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    _server = std::make_unique<QTcpServer>();
 
-    //if (_server_fd < 0)
-    //    return { false, "Error: Failed to create server socket" };
+    connect(_server.get(), &QTcpServer::newConnection, this, &CollabServer::on_new_connection);
 
-    //sockaddr_in server_addr;
-    //server_addr.sin_family = AF_INET;
-    //server_addr.sin_addr.s_addr = INADDR_ANY;
-    //server_addr.sin_port = htons(port);
+    if (!_server->listen(QHostAddress::Any, port)) {
+        return { false, "Error: Failed to listen on server socket" };
+    }
 
-    //if (bind(_server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
-    //    return { false, "Error: Failed to bind server socket" };
-
-    //if (listen(_server_fd, 5) < 0)
-    //    return { false, "Error: Failed to listen on server socket" };
-
-    return { true, "Success: Server created" };
+    return { true, "Success: Server created on port " + std::to_string(port) };
 }
 
 CollabServer::statusRes CollabServer::startServerThread(int port) {
@@ -141,11 +172,8 @@ CollabServer::statusRes CollabServer::startServerThread(int port) {
     if (!status.code)
         return status;
 
-    std::thread server_thread([this]() {
-        this->check_peers_connection();
-    });
-    server_thread.detach();
+    // Note: Qt signal/slot mechanism handles threading automatically through the event loop
+    // No explicit thread creation needed
 
     return status;
 }
-*/
