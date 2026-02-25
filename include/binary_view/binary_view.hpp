cfdf2a35625de17529fdee6130f13e7c19e27959
Clang Robot <robot@example.com>
2026-02-25 23:36:04 +0000

    :art: Committing clang-format changes

--- include/binary_view/binary_view.hpp
+++ include/binary_view/binary_view.hpp
@@ -1,30 +1,29 @@
 #pragma once
 
-#include "binary/binary.hpp"
 #include <ranges>
 
+#include "binary/binary.hpp"
+
 struct CheckPoint {
-	size_t index;
-	uintptr_t addr;
+    size_t index;
+    uintptr_t addr;
 };
 
 class BinaryView {
-public:
-	BinaryView(Binary* binary);
-	~BinaryView() = default;
-	std::vector<uint8_t> getSectionContentFromAddr(
-		size_t addr, size_t content_size);
-	std::string viewHexChunk(
-		size_t addr);
-	std::string viewDisasmChunk(
-		size_t addr);
-	BinSection* getSectionAtAddr(uintptr_t virtual_addr);
-	BinSection* getNextSection(BinSection* section);
+   public:
+    BinaryView(Binary* binary);
+    ~BinaryView() = default;
+    std::vector<uint8_t> getSectionContentFromAddr(size_t addr,
+                                                   size_t content_size);
+    std::string viewHexChunk(size_t addr);
+    std::string viewDisasmChunk(size_t addr);
+    BinSection* getSectionAtAddr(uintptr_t virtual_addr);
+    BinSection* getNextSection(BinSection* section);
 
-private:
-	std::pair<uintptr_t, CheckPoint> getDisasmInstructionAddr(size_t index);
+   private:
+    std::pair<uintptr_t, CheckPoint> getDisasmInstructionAddr(size_t index);
 
-private:
-	Binary* _binary;
-	uintptr_t _relative_text_addr = 0x1000;
+   private:
+    Binary* _binary;
+    uintptr_t _relative_text_addr = 0x1000;
 };
